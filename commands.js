59b8a8b2a0eb7189ec28149fc2c15de16b0c8b4d
Eagle <eagle@eaglehunt.net>
2023-06-12 18:52:17 -0700

    working

--- commands.js
+++ commands.js
@@ -1,8 +1,12 @@
 const calls = require("./api_calls.json");
 const secrets = require("./secrets.json");
 const readline = require("readline");
+const clipboard = require("clipboardy");
 
 class GameStateCommands {
+  constructor() {
+    this.getFactionInfo(); // Call the method in the constructor
+  }
   static #factionHeader = {
     headers: {
       "Content-Type": "application/json",
@@ -10,14 +14,14 @@ class GameStateCommands {
     },
   };
 
-  static factionInfo = {
+  factionInfo = {
     names: [],
     symbol: [],
     description: [],
   };
 
-  static getFactionInfo = () => {
-    fetch(calls.factions, this.#factionHeader)
+  getFactionInfo = () => {
+    fetch(calls.factions, GameStateCommands.#factionHeader)
       .then((response) => response.json())
       .then((response) => {
         for (let i = 0; i < response.data.length; i++) {
@@ -29,13 +33,13 @@ class GameStateCommands {
       .catch((err) => console.error(err));
   };
 
-  static displayFactions = () => {
+  displayFactions = () => {
     for (let i = 0; i < this.factionInfo.names.length; i++) {
       console.log(`${i}: ${this.factionInfo.names[i]}`);
     }
   };
 
-  static #getAgentSign = () => {
+  #getAgentSign = () => {
     return new Promise((resolve) => {
       const rl = readline.createInterface({
         input: process.stdin,
@@ -49,7 +53,7 @@ class GameStateCommands {
     });
   };
 
-  static #getAgentFaction = () => {
+  #getAgentFaction = () => {
     return new Promise((resolve) => {
       const rl = readline.createInterface({
         input: process.stdin,
@@ -68,31 +72,82 @@ class GameStateCommands {
     });
   };
 
-  static register = async () => {
+  validateRegister = async (info) => {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(info),
+    };
+
+    try {
+      const response = await fetch(calls.registerUser, options);
+      const data = await response.json();
+
+      if (data.error) {
+        console.log(data.error.data.symbol[0]);
+        return false;
+      } else {
+        console.log(
+          `Registered! Here is your access token. It has been copied to your clipboard\n${data.data.token}\n`
+        );
+        clipboard.writeSync(data.data.token);
+        return true;
+      }
+    } catch (err) {
+      console.error(err);
+      return false;
+    }
+  };
+
+  register = async () => {
     let info = {
       symbol: "",
       faction: "",
     };
 
-    info.symbol = await this.#getAgentSign();
+    const symbol = await this.#getAgentSign();
+    info.symbol = symbol;
 
-    console.log(`Welcome, ${info.symbol}`);
+    console.log(`Welcome, ${symbol}`);
     console.log("It is now time to join a faction. Choose wisely.\n");
 
     this.displayFactions();
 
-    let confirm = false;
+    let validFactionSelected = false;
 
-    while (!confirm) {
-      let faction = await this.#getAgentFaction();
+    while (!validFactionSelected) {
+      let factionNumber = await this.#getAgentFaction();
+      factionNumber = parseInt(factionNumber);
 
-      if (faction <= this.factionInfo.names.length - 1) {
+      if (factionNumber <= this.factionInfo.names.length - 1) {
+        info.faction = this.factionInfo.symbol[factionNumber];
+
+        let registered = false;
+
+        while (!registered) {
+          registered = await this.validateRegister(info);
+
+          if (!registered) {
+            info.symbol = await this.#getAgentSign();
+          } else {
+            break; // Break the loop when registration is successful
+          }
+        }
+
+        validFactionSelected = true;
       } else {
-        console.log("Incorrect input. Please enter a correct faction.");
+        console.log("Incorrect input. Please enter a listed faction.");
       }
     }
   };
+
+  login = () => {
+    /* continue */
+  };
 }
 
-GameStateCommands.getFactionInfo();
-GameStateCommands.register();
+const game = new GameStateCommands();
+
+game.register();
